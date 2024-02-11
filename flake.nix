1b473d9a08f2ac3ed3cde7bf1ee43681f82d1dc7
Wilfred Allyn <wilfredallyn@protonmail.com>
2024-02-11 09:57:30 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "pyasic";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.myapp;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.myapp ];
+          packages = [ pkgs.poetry pkgs.sqlite];
+        };
+      });
+}
