f34010178424fc1d25612f6948d4f000d69e0215
Wilfred Allyn <wilfredallyn@protonmail.com>
2024-02-17 04:59:46 -0500

    add hashrate fig

--- pyasic/dashboard/pages/hashrate.py
+++ pyasic/dashboard/pages/hashrate.py
@@ -1,11 +1,40 @@
 import dash
-from dash import html, dash_table
+from dash import callback, dcc, html
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
+from io import StringIO
+import json
+from pyasic.viz import get_hashrate_fig
+import pandas as pd
 
 
 dash.register_page(__name__, path="/hashrate", name="Hashrate")
 
 
 def layout():
-    layout = html.Div([dbc.Container([dbc.Row([html.P(["hashrate"])])])])
-    return layout
+    return html.Div(
+        [
+            dbc.Container(
+                [
+                    dbc.Row(
+                        [
+                            dcc.Graph(id="hashrate-graph"),
+                        ]
+                    )
+                ]
+            ),
+            dcc.Store(id="data-store"),
+        ]
+    )
+
+
+@callback(Output("hashrate-graph", "figure"), [Input("data-store", "data")])
+def update_hashrate_graph(data):
+    if data is None:
+        return {}
+
+    df_json = json.loads(data)
+    df = pd.read_json(StringIO(df_json), orient="split")
+
+    fig = get_hashrate_fig(df)
+    return fig
--- pyasic/viz/__init__.py
+++ pyasic/viz/__init__.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
@@ -22,5 +23,34 @@ def get_status_fig(df):
     fig.update_yaxes(title_text="Temperature", secondary_y=True)
     fig.update_yaxes(range=[0, 50], secondary_y=False)
     fig.update_yaxes(range=[0, 100], secondary_y=True)
+
+    return fig
+
+
+def get_hashrate_fig(df):
+    avg_hashrate = (
+        df[["hashboards_0_hashrate", "hashboards_1_hashrate", "hashboards_2_hashrate"]]
+        .mean(axis=0)
+        .sum()
+    )
+
+    fig = px.line(
+        df,
+        x="datetime",
+        y=["hashboards_0_hashrate", "hashboards_1_hashrate", "hashboards_2_hashrate"],
+        labels={"value": "Hashrate", "variable": "Hashboards"},
+        title="Hashrate over Time",
+    )
+
+    for trace, new_name in zip(fig.data, ["0", "1", "2"]):
+        trace.name = new_name
+
+    fig.add_hline(
+        y=avg_hashrate,
+        line_dash="dot",
+        annotation_text=f"Average Hashrate ({avg_hashrate.round(2)} TH/s)",
+        annotation_position="bottom right",
+    )
+    fig.update_yaxes(range=[0, avg_hashrate * 1.1])
 
     return fig
